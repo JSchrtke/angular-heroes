0b384d38d6b95c921de9b3ebb7749edec84cb975
jschrtke <jschrtke@proton.me>
2023-05-12 15:43:26 +0200

    chore: reconfigure formatting & linting && format code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,16 @@
-import { Component } from "@angular/core";
-import { Hero, HEROES } from "../hero";
+import { Component } from '@angular/core';
+import { Hero, HEROES } from '../hero';
 
 @Component({
-    selector: "app-heroes",
-    templateUrl: "./heroes.component.html",
-    styleUrls: ["./heroes.component.css"],
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-    heroes = HEROES;
+  heroes = HEROES;
 
-    selectedHero?: Hero;
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
