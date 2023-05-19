6d19ddd72a6b378c4f1dc1fc203b4bd8b923d372
jschrtke <jschrtke@proton.me>
2023-05-19 11:22:05 +0200

    feat: get heroes via HTTP request

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,24 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { Hero, HEROES } from './hero';
+import { catchError, Observable, of } from 'rxjs';
+import { Hero } from './hero';
 import { MessageService } from './message.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = '/api/heroes';
+  constructor(private httpClient: HttpClient, private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
-    console.log('before');
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.httpClient.get<Hero[]>(this.heroesUrl).pipe(catchError(this.handleError<Hero[]>('getHeroes', [])));
+  }
+
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      this.messageService.add(`HeroService: ${operation} failed: ${error}`);
+      return of(result as T);
+    };
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find((hero) => hero.id == id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    return this.httpClient
+      .get<Hero>(`${this.heroesUrl}/${id}`)
+      .pipe(
+        tap((_) => this.messageService.add(`fetched hero id=${id}`), catchError(this.handleError<Hero>('getHero')))
+      );
   }
 }
