64519eab20fbc58871fe24b995a83623dc30d630
jschrtke <jschrtke@proton.me>
2023-05-26 11:19:55 +0200

    chore: fix linting errors

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,7 +21,7 @@ export class HeroService {
   }
 
   handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
+    return (error: unknown): Observable<T> => {
       console.log(error);
       this.log(`${operation} failed: ${error}`);
       return of(result as T);
@@ -33,27 +33,41 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
-    return this.httpClient
-      .get<Hero>(`${this.heroesUrl}/${id}`)
-      .pipe(tap(() => this.log(`fetched hero id=${id}`), catchError(this.handleError<Hero>('getHero'))));
+    return this.httpClient.get<Hero>(`${this.heroesUrl}/${id}`).pipe(
+      tap({
+        complete: () => this.log(`fetched hero id=${id}`),
+        error: () => catchError(this.handleError<Hero>('getHero')),
+      })
+    );
   }
 
   updateHero(hero: Hero): Observable<Hero> {
-    const response = this.httpClient
-      .post<Hero>(`${this.heroesUrl}/${hero.id}`, hero, this.httpOptions)
-      .pipe(tap(() => this.log(`updated hero id=${hero.id}`), catchError(this.handleError<Hero>('updateHero'))));
+    const response = this.httpClient.post<Hero>(`${this.heroesUrl}/${hero.id}`, hero, this.httpOptions).pipe(
+      tap({
+        complete: () => this.log(`updated hero id=${hero.id}`),
+        error: () => catchError(this.handleError<Hero>('updateHero')),
+      })
+    );
     return response;
   }
 
   addHero(hero: Hero): Observable<Hero> {
-    return this.httpClient
-      .post<Hero>(`${this.heroesUrl}/${hero.id}`, hero)
-      .pipe(tap(() => this.log(`added hero with id=${hero.id}`), catchError(this.handleError<Hero>('addHero'))));
+    return this.httpClient.post<Hero>(`${this.heroesUrl}/${hero.id}`, hero).pipe(
+      tap({
+        complete: () => this.log(`added hero with id=${hero.id}`),
+        error: () => catchError(this.handleError<Hero>('addHero')),
+      })
+    );
   }
 
-  deleteHero(heroId: number): Observable<any> {
-    return this.httpClient
-      .delete<Hero>(`${this.heroesUrl}/${heroId}`)
-      .pipe(tap(() => this.log(`deleted hero with id=${heroId}`), catchError(this.handleError<Hero>('addHero'))));
+  deleteHero(heroId: number): Observable<unknown> {
+    return this.httpClient.delete<Hero>(`${this.heroesUrl}/${heroId}`).pipe(
+      tap({
+        complete: () => {
+          this.log(`deleted hero with id=${heroId}`);
+        },
+        error: () => catchError(this.handleError<Hero>('addHero')),
+      })
+    );
   }
 }
